34010b29ebec27da5aac48af917eb877172d07ea
lonelymj <511639411@qq.com>
2013-08-03 00:39:31 +0800

    删错

---
+++ ExtAdmin/BLL/DataBaseManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json.Linq;
+using System.Text;
+using System.Data.SqlClient;
+	
+namespace ExtAdmin.BLL
+{
+public class DataBaseManager
+{
+	public static JObject Info(string conStr) {
+    bool b =false;
+	    string server = "";
+	    string userName = "";
+	    if (conStr != "")
+	    {
+	        b = true;
+	        byte[] by = Convert.FromBase64String(conStr);
+	        conStr = Encoding.Default.GetString(by);
+	        SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder(conStr);
+	        server = sb.DataSource;
+	        userName = sb.UserID;
+	    }
+	    return new JObject(new JProperty[] { new JProperty("success", b), new JProperty("server", server), new JProperty("userName", userName) });
+	}
+}
+}
---
+++ ExtAdmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExtAdmin.Global" Language="C#" %>
